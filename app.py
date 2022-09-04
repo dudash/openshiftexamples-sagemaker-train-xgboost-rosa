8e6897031fc023dc8b706e1628f063e391e28445
Jason Dudash <thenut77@gmail.com>
2022-09-04 19:00:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,30 +14,28 @@ from pydantic import BaseModel
 #model = pickle.load(open("xgboost-model", "rb"))
 model = joblib.load("xgboost-model")
 
-
 def create_type_instance(type_name: str):
     return locate(type_name).__call__()
 
 # This function will vary depending on your model
 # xgboost lets us extract both the feature names and a string value of the feature type
 def get_features_dict(model):
-    feature_names = model.get_booster().feature_names
-    feature_types = list(map(create_type_instance, model.get_booster().feature_types))
+#    feature_names = model.get_booster().feature_names
+#    feature_types = list(map(create_type_instance, model.get_booster().feature_types))
+    feature_names = model.feature_names
+    feature_types = list(map(create_type_instance, model.feature_types))
     return dict(zip(feature_names, feature_types))
 
 # get the class types from our features and type strings
 def create_input_features_class(model):
     return type("InputFeatures", (BaseModel,), get_features_dict(model))
 
-
 InputFeatures = create_input_features_class(model)
 app = FastAPI()
-
 
 @app.post("/predict", response_model=List)
 async def predict_post(datas: List[InputFeatures]):
     return model.predict(np.asarray([list(data.__dict__.values()) for data in datas])).tolist()
-
 
 if __name__ == "__main__":
     print(get_features_dict(model))
